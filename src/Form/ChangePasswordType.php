963fe5c2b36bd4c4e72787a97824ff85d4579fac
Michel Delahaye <m_delahy@club-internet.fr>
2021-03-26 19:28:03 +0100

    Ajout Modifier le password

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Nom'
+            ])
+            ->add('email', EmailType::class, [
+                'disabled' => true
+            ])
+            ->add('old_password', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Votre Mot de passe actuel',
+                    
+                ]
+            ])
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'Le password et la confirmation du password doivent être identique.',
+                'label' => 'Nouveau mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Nouveau mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre not de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmer votre mot de passe'
+                    ]
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
