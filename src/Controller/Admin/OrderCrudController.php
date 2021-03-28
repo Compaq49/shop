989add4340b49d1ec1ce32bb75fb9ac0b2d02e62
Michel Delahaye <m_delahy@club-internet.fr>
2021-03-28 20:55:37 +0200

    Mapping de l'entity Order avec EasyAdmin

---
+++ src/Controller/Admin/OrderCrudController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class OrderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Order::class;
+    }
+    public function configureActions(Actions $actions): Actions 
+    {
+	return $actions
+		->add('index', 'detail')   // on ajoute le lien voir
+                	->disable(Action::EDIT, Action::DELETE); //on supprime les liens edit et supprimer
+    }
+
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id', "Id"),    
+            DateField::new('createdAt', "Passée le"),
+            TextField::new('user.getFullName', 'Utilisateur'),
+            MoneyField::new('total')->setCurrency('EUR'),
+            //TextEditorField::new('delivery', 'Adresse de livraison')->onlyOnDetail(),
+            MoneyField::new('total', 'Total commande')->setCurrency('EUR'),
+            //TextField::new('carrierName', 'Transporteur'),
+        ];
+    }
+    
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -143,6 +143,11 @@ class User implements UserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+    
+    public function getFullName(): string 
+    {
+        return $this->getFirstname().' '.$this->getLastname();
+    }
 
     public function getFirstname(): ?string
     {
