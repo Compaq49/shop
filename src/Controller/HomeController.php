da8b4b97a2bf028b9e1bd050af47e16ab6cedcc0
Michel Delahaye <m_delahy@club-internet.fr>
2021-04-10 22:04:08 +0200

    Ajout des meilleures ventes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+    
+    public function __construct(EntityManagerInterface $entityManager) 
+    {
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $products = $this->entityManager->getRepository(\App\Entity\Product::class)->findByIsBest(1);
+        
+        return $this->render('home/index.html.twig', [
+            'products' => $products,
+        ]);
     }
 }
